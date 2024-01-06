1a8964ad2d50cd99c26c09dd7cedf1a30300bed5
Banik <chiradeep13@gmail.com>
2024-01-06 18:00:35 +0530

    [NVIM] setup

---
+++ run_once_2_nvim_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+useSudo='sudo'
+
+if [ "$(id -u)" -eq 0 ]; then
+    useSudo=''
+fi
+
+updg() {
+    local useSudo=$1
+
+    $useSudo apt-get update >/dev/null && $useSudo apt-get upgrade -y >/dev/null;
+    if [ $? -eq 0 ]; then
+        echo "Update Done"
+    fi
+    return $?
+}
+
+nvim_setup() {
+    git clone --depth=1 https://github.com/neovim/neovim /tmp/neovim
+    cd /temp/neovim && make -j 4 CMAKE_BUILD_TYPE=RelWithDebInfo
+    cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+    
+    if [[ $? -eq 0 ]]; then
+        echo "NVIM installed"
+    fi
+
+}
+
+updg $useSudo
+
+
+nvim_setup
